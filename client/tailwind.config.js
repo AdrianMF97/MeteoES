b4a4ebb967f9e3c9b25c9cc98b5e2e8786131e99
Adrian <adrianmartinezfdez1997@gmail.com>
2026-02-06 13:22:53 +0100

    New font : "Outfit"

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,22 @@ export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Outfit",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "sans-serif",
+        ],
+      },
+    },
   },
   plugins: [],
 };
